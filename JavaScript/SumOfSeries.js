bbecfdcb15559a5a8dd7c4069406f9ab6f4186c3
Your Name <your.email@example.com>
2025-09-17 12:52:29 +0530

    js

---
+++ JavaScript/SumOfSeries.js
@@ -0,0 +1,8 @@
+var n,i=1,sum=0;
+    n = parseInt(prompt("Enter N value : "));
+    for(i=1;i<=n;i++)
+    {
+        sum = sum+i;
+    }
+    document.write("<h2> The Sum of Series is : "+sum+"</h2>");
+    alert("The Sum of Sreies is : "+sum);
